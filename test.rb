6bfc3774c0d443068b3201afc17a6048d2da9d6a
mahmoud Shahin <elkont33@gmail.com>
2019-02-24 15:25:12 +0200

    remembering variables

--- test.rb
+++ test.rb
@@ -1,3 +1,12 @@
 puts "  / |     /|     /|   "
 puts " /  |    / |    / |   "
-puts"/___|   /__|   /__|   "
+puts "/___|   /__|   /__|   "
+
+# this is where to store the variables
+name = " john"
+age = 32 
+
+#start of the story 
+
+puts "His name was #{name}"
+puts " He was #{age} years old"
