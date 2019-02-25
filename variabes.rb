1a3472eec7f985e2a6b872ce52ea585284c0baac
mahmoud Shahin <elkont33@gmail.com>
2019-02-25 16:10:37 +0200

    More on variables

---
+++ variabes.rb
@@ -0,0 +1,10 @@
+character_name = " Mahmoud "
+age = 26
+height =178 
+weight = 125
+
+puts " Stay strong #{character_name} you are #{age} and you need to stay strong at all times "
+number_of_letters = character_name.length()
+search_result= character_name.include? "M"
+puts number_of_letters
+puts search_result
