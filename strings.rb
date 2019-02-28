f7a8b9d61fbf664ac5220473b6ebd34ba6d90922
mahmoud Shahin <elkont33@gmail.com>
2019-02-28 05:38:50 +0200

    Strings somemore

---
+++ strings.rb
@@ -0,0 +1,6 @@
+puts " he said \"this is a quatation\" and smiled"
+puts " and then he said \n \"I will be back\""
+phrase = "   about the terminator " 
+puts phrase.strip()
+puts "this include this number of letters:" 
+puts phrase.length
